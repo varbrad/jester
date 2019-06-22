54865f7ee6d5e2325297b21aefaf5aaa56df60d4
Brad Davies <varbrad@users.noreply.github.com>
2019-06-22 10:57:24 +0100

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+const ERROR = 2
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ]
+}
